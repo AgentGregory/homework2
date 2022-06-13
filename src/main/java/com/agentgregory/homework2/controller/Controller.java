59fd10764b8f24a2a4ce5b36c2ed070ac5cdee25
AgentGregory <grege2005@gmail.com>
2022-06-13 23:34:37 +0400

    fix controller

--- src/main/java/com/agentgregory/homework2/controller/Controller.java
+++ src/main/java/com/agentgregory/homework2/controller/Controller.java
@@ -6,28 +6,29 @@ import com.agentgregory.homework2.repository.FileNodeRepository;
 import com.agentgregory.homework2.repository.FileNodeRepositoryService;
 import com.agentgregory.homework2.service.FileComputer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.util.List;
 
 
-@org.springframework.stereotype.Controller
+@RestController
 @RequiredArgsConstructor
 public class Controller {
     private final FileComputer fileComputer;
     private final FileNodeRepositoryService fileNodeRepositoryService;
 
-    @GetMapping("/localhost/files/fill")
-    public FileNode fillDB (@RequestBody File rootPath, Long id){
-        return fileComputer.searchFileNode(rootPath, id);
+    @PostMapping("/localhost/files/fill")
+    ResponseEntity<FileComputer> searchFileNode(@RequestBody File rootPath, Long id) {
+        fileComputer.searchFileNode(rootPath, id);
+        return new ResponseEntity<FileComputer>(HttpStatus.CREATED);
     }
 
     @GetMapping(" /localhost/files/find?name=")
-    public String searchName(@RequestBody String name, String rootPath){
-        return fileNodeRepositoryService.constructPath(name, rootPath);
-
+    ResponseEntity<String> constructPath(@RequestBody String name, String rootPath) {
+        final String constructPath = fileNodeRepositoryService.constructPath(name, rootPath);
+        return new ResponseEntity<> (constructPath, HttpStatus.OK);
     }
 }
