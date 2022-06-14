559e3e570e580f3349ff2444000797a14bbc5f22
AgentGregory <grege2005@gmail.com>
2022-06-14 18:45:42 +0400

    fix controller

--- src/main/java/com/agentgregory/homework2/controller/Controller.java
+++ src/main/java/com/agentgregory/homework2/controller/Controller.java
@@ -2,33 +2,28 @@ package com.agentgregory.homework2.controller;
 
 import com.agentgregory.homework2.model.FileNode;
 import com.agentgregory.homework2.model.FileNodeEntity;
-import com.agentgregory.homework2.repository.FileNodeRepository;
 import com.agentgregory.homework2.repository.FileNodeRepositoryService;
-import com.agentgregory.homework2.service.FileComputer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.util.List;
-
-
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/files")
 public class Controller {
-    private final FileComputer fileComputer;
     private final FileNodeRepositoryService fileNodeRepositoryService;
 
-    @PostMapping("/localhost/files/fill")
-    ResponseEntity<FileComputer> searchFileNode(@RequestBody File rootPath, Long id) {
-        fileComputer.searchFileNode(rootPath, id);
-        return new ResponseEntity<FileComputer>(HttpStatus.CREATED);
+
+    @PostMapping("/fill")
+    public ResponseEntity<?> save(@RequestBody FileNode fileNode) {
+        fileNodeRepositoryService.save(fileNode);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping(" /localhost/files/find?name=")
-    ResponseEntity<String> constructPath(@RequestBody String name, String rootPath) {
-        final String constructPath = fileNodeRepositoryService.constructPath(name, rootPath);
-        return new ResponseEntity<> (constructPath, HttpStatus.OK);
+    @GetMapping("/find?name=")
+    ResponseEntity<?> constructPath(@RequestParam String name, String rootPath) {
+        String constructPath = fileNodeRepositoryService.constructPath(name, rootPath);
+        return new ResponseEntity<>(constructPath, HttpStatus.OK);
     }
 }
